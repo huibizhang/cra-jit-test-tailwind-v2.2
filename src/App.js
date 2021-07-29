9d04783a70958fb15184161161112d2f7cf703b5
搋兔 <naruto6404@hotmail.com>
2021-07-30 07:00:04 +0800

    update: contents of template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import DoubleSlider from "./DoubleSlider";
+import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="">Hello</h1>
-      <DoubleSlider />
+    <div className="text-center">
+      <header className="bg-[#282c34] min-h-screen flex flex-col justify-center text-xl text-white">
+        <img src={logo} className="h-[40vmin] mx-auto pointer-events-none animate-spin animate-speed" alt="logo" />
+        <style>
+          {"\
+            .animate-speed{\
+              animation-duration:20s;\
+            }\
+          "}
+        </style>
+        <p>
+          Edit <code className="text-gray-400">src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="text-[#61dafb] hover:text-[#b3eeff] transition-all"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
